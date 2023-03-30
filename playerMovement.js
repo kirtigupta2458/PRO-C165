290af4a7a2a4cce4738adf86005a9c0061cfae1b
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2023-03-30 15:31:57 +0530

    Update playerMovement.js

--- playerMovement.js
+++ playerMovement.js
@@ -4,15 +4,7 @@ AFRAME.registerComponent("player-movement", {
   },
   walk: function () {
     window.addEventListener("keydown", (e) => {
-      if (
-        e.key === "ArrowUp" ||
-        e.key === "ArrowRight" ||
-        e.key === "ArrowLeft" ||
-        e.key === "ArrowDown"
-      ) {
-        var entity = document.querySelector("#sound2");
-        entity.components.sound.playSound();
-      }
+      
     });
   },
 });
